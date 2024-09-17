a080bd86c141082682d723b3ecb309fe7f848024
kamenyanislavov <165830655+kamenyanislavov@users.noreply.github.com>
2024-09-17 17:13:26 +0300

    Add files via upload

---
+++ sumNumbersM-M.js
@@ -0,0 +1,18 @@
+function sumOfNumbers(n, m) {
+
+    let num1 = Number(n);
+    let num2 = Number(m);
+    let sum = 0;
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
+    
+
+}
+
+sumOfNumbers('1', '5');
+console.log('---------------');
+sumOfNumbers('-8', '20');
